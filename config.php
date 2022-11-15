b8698410e1a3880df484ac01c9afe3158c56d491
Alireza Naseri <alireza.real98@gmail.com>
2022-11-16 01:36:17 +0330

    add Configuration class for DataBase Connection

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+class Config{
+ 
+  // specify your own database credentials
+  private $host = "localhost";
+  private $username = "root";
+  private $password = "";
+  private $dbName;
+  public $connection;
+
+  public function __construct($dbName){
+    $this->dbName = $dbName;
+  }
+
+  // get the database connection
+  public function dbConnection(){
+
+      $this->connection = null;
+
+      try{
+          $this->connection = new PDO("mysql:host=". $this->host .";dbname=" . $this->dbName, $this->username, $this->password);
+          // set the PDO error mode to exception
+          $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
+      }catch(PDOException $exception){
+        echo "Connection failed: " . $exception->getMessage();
+      }
+
+      return $this->connection;
+  }
+}
+
+?>
