5ba38622ddab5e1899a4561bbba2ad6359317cc2
Alireza Naseri <alireza.real98@gmail.com>
2022-11-16 23:03:36 +0330

    add a comment in the code

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ include_once 'config.php';
 // If this were true, $_POST['submit'] is set
 if (isset($_POST['submit']))
 {
+    // Create instance of Config class
     $configuration = new Config($_POST['dbname']);
+    // Create and Connect to Database
     $db = $configuration->dbConnection();
 
 }
