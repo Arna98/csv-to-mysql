662c7a302875ff2b983632650806c1a9ba686e74
Alireza Naseri <alireza.real98@gmail.com>
2022-11-18 01:12:35 +0330

    specify the name of the table for database

--- index.php
+++ index.php
@@ -6,10 +6,22 @@ include_once 'config.php';
 if (isset($_POST['submit']))
 {
     // Create instance of Config class
-    $configuration = new Config($_POST['dbname']);
+    $configuration = new Config($_POST['dbName']);
     // Create and Connect to Database
     $db = $configuration->dbConnection();
 
+    $numOfTbl = $_POST['numberOfTbl'];
+
+    for ($count=1; $i <= $numOfTbl; $count++) 
+    { 
+        $sourceFile = $_FILES["csvFile$count"]["name"];
+
+        // Specify the name of the table
+        $tblName = $_POST["tblName$count"];
+        $tblName = $tblName . "_" . str_replace(".csv","", $sourcFile);
+        
+    }
 }
+
 
 ?>
